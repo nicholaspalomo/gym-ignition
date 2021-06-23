0ff079da62780a3864f1fd10fab2de3b082018df
Nico Palomo <npalomo@student.ethz.ch>
2021-06-23 01:27:42 -0700

    added init.py scripts

--- gym_ignition/gym_ignition/env/env/panda/__init__.py
+++ gym_ignition/gym_ignition/env/env/panda/__init__.py
@@ -1,4 +1,4 @@
 import os
 from _gym_ignition import *
 
-__SPOT_RESOURCE_DIRECTORY__ = os.path.dirname(os.path.realpath(__file__))+'/rsc'
+__PANDA_RESOURCE_DIRECTORY__ = os.path.dirname(os.path.realpath(__file__))+'/rsc'
